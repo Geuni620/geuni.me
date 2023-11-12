ddb60c08b6e186f35a3e50bf51d5e5c3b5761a65
Geuni <dlrmsgnl620@naver.com>
2023-11-12 17:43:19 +0900

    Feat: add style

--- app/components/nav/index.tsx
+++ app/components/nav/index.tsx
@@ -2,7 +2,7 @@ import { DarkModeToggle } from './DarkModeToggle';
 
 export const Nav = () => {
   return (
-    <ul className="flex items-center justify-between">
+    <ul data-animate className="flex items-center justify-between">
       <div className="flex gap-2">
         <li>이근휘</li>
         <li>Geuni620</li>
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 export default function Home() {
   return (
-    <div>
-      <div className="my-7 select-none">
+    <div data-animate data-animate-speed="slow">
+      <div className="mb-7 mt-14 select-none">
         <p>기록하는 공간이 필요했습니다.</p>
         <p>머리가 좋지 못해, 머리에 다 담기엔 생각이 너무 많습니다.</p>
         <p>공간을 만들어 짧게라도 기록하려 합니다.</p>
       </div>
 
-      <div className="my-14 flex gap-7">
+      <div data-animate className="mb-7 mt-14 flex select-none gap-7">
         <p>공간</p>
-
         <ul>
           <li className="flex items-center justify-center">
             <p className="font-bold">수첩</p>
