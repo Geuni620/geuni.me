4cad00ea84c2b28fd3554b1a7400d2d84afc2d79
이근휘 <dlrmsgnl620@naver.com>
2023-11-22 14:04:50 +0900

    WIP: slug, list layout

---
+++ app/(router)/[id]/[slug]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Slug = () => {
+  return <div>여기는 slug</div>;
+};
+
+export default Slug;
--- app/(router)/[id]/page.tsx
+++ app/(router)/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { compareDesc } from 'date-fns';
 import { format, parseISO } from 'date-fns';
-import React from 'react';
+import Link from 'next/link';
 
 import { allPosts } from '@/.contentlayer/generated';
 
@@ -9,15 +9,19 @@ const ListPage = () => {
     compareDesc(new Date(a.createdAt), new Date(b.createdAt)),
   );
 
+  console.log(posts[0]._raw);
+
   return (
     <div className="mb-7 mt-14">
       {posts.map((post) => (
         <>
           {/* 2023 / 2022 / 2021 묶어줘야함 */}
           <div className="mt-2 flex justify-between">
-            <h2 className="text-[22px]" key={post._id}>
-              {post.title}
-            </h2>
+            <Link href={`/posts/${post._raw.flattenedPath}`} className="">
+              <h2 className="text-[22px]" key={post._id}>
+                {post.title}
+              </h2>
+            </Link>
             <time dateTime={post.createdAt}>
               {format(parseISO(post.createdAt), 'MM.dd')}
             </time>
