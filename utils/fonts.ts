513a6dae3fec984d91aea916671d112fca4d64ef
Geuni <dlrmsgnl620@naver.com>
2023-11-25 16:12:00 +0900

    chore: font error solve

--- utils/fonts.ts
+++ utils/fonts.ts
@@ -1,8 +1,6 @@
 import {
   Inter,
   Nanum_Brush_Script,
-  Nanum_Gothic,
-  Nanum_Pen_Script,
   Noto_Sans_KR,
   Open_Sans,
 } from 'next/font/google';
@@ -12,21 +10,7 @@ export const notoSansKR = Noto_Sans_KR({
   weight: ['400', '700'],
 });
 
-/**
- * @description error 발생 → nanum brush script 변경
- */
-export const nanumPenScript = Nanum_Pen_Script({
-  subsets: ['latin'],
-  weight: ['400'],
-  display: 'swap',
-});
-
 export const inter = Inter({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-});
-
-export const nanumGothic = Nanum_Gothic({
   subsets: ['latin'],
   weight: ['400', '700'],
 });
@@ -36,22 +20,7 @@ export const nanumBrushScript = Nanum_Brush_Script({
   weight: ['400'],
 });
 
-export const nanumBarunGothic = Nanum_Gothic({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-});
-
 export const openSans = Open_Sans({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-});
-
-export const notoSans = Noto_Sans_KR({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-});
-
-export const notoSerif = Noto_Sans_KR({
   subsets: ['latin'],
   weight: ['400', '700'],
 });
