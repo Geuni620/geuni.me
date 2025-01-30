8990ae1ef78146efd9eaa09b616a3ed01cfc0b95
Geuni <dlrmsgnl620@naver.com>
2025-01-31 00:11:49 +0900

    WIP: main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import { PostCard } from "@/components/post-card";
+import { Container } from "@/components/layout";
 
 export default function Home() {
   return (
-    <div>
-      <PostCard title="title" description="description" href="/" />
-    </div>
+    <Container>
+      <div className="space-y-16">
+        <section className="space-y-4">
+          <PostCard title="title" description="description" href="/" />
+        </section>
+      </div>
+    </Container>
   );
 }
