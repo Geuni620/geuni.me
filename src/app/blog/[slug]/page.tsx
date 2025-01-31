def6774655148a94ce6b8f1f5dda800dab58123c
Geuni <dlrmsgnl620@naver.com>
2025-02-01 02:26:26 +0900

    feat: add dayjs reading time

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -4,7 +4,7 @@ import { Container } from "@/components/layout";
 import { CONTENT_PATH } from "@/constants/config";
 import { PostBody } from "@/components/post-body";
 import { PostHeader } from "@/components/post-header";
-
+import readingTime from "reading-time";
 export const generateStaticParams = async () => {
   const files = fs.readdirSync(CONTENT_PATH);
 
@@ -21,11 +21,16 @@ export default async function Page({
   const { slug } = await params;
   const fileContent = fs.readFileSync(`${CONTENT_PATH}/${slug}.mdx`, "utf8");
   const { data, content } = matter(fileContent);
+  const readingMinutes = Math.ceil(readingTime(content).minutes);
 
   return (
     <Container>
       <article className="prose dark:prose-invert">
-        <PostHeader title={data.title} date={data.date} />
+        <PostHeader
+          title={data.title}
+          date={data.date}
+          readingTime={readingMinutes}
+        />
         <PostBody content={content} />
       </article>
     </Container>
--- src/components/post-header.tsx
+++ src/components/post-header.tsx
@@ -1,16 +1,32 @@
+import dayjs from "dayjs";
+
 interface PostHeaderProps {
   title: string;
   date: string;
-  categories: string[] | undefined;
-  summary: string | undefined;
-  thumbnail: string | undefined;
+  readingTime?: number;
 }
 
-export const PostHeader = ({ title, date }: PostHeaderProps) => {
+interface FormattedDateProps {
+  date: string | Date;
+  readingTime?: number;
+}
+
+export const PostHeader = ({ title, date, readingTime }: PostHeaderProps) => {
   return (
     <div>
       <h1>{title}</h1>
-      <FormattedDate date={date} />
+      <FormattedDate date={date} readingTime={readingTime} />
     </div>
+  );
+};
+
+export const FormattedDate = ({ date, readingTime }: FormattedDateProps) => {
+  const formattedDate = dayjs(date).format("MMM D");
+
+  return (
+    <time dateTime={dayjs(date).toISOString()}>
+      {formattedDate}
+      {readingTime && ` · ${readingTime}min`}
+    </time>
   );
 };
