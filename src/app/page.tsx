bcb6c872c5ac82298f4d40a1246bb0fd48ba9f73
Geuni <dlrmsgnl620@naver.com>
2025-02-01 15:50:13 +0900

    WIP: main grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,34 +7,34 @@ export default async function Home() {
   const postsByYear = await getPostByYear();
   const years = Object.keys(postsByYear).sort((a, b) => b.localeCompare(a));
 
-  console.log(years);
-  console.log(postsByYear);
-
   return (
     <Container>
-      <div className="space-y-16">
-        {years.map((year) => {
-          return (
-            <div key={year} className="grid grid-cols-[100px_1fr_100px] gap-4">
-              {postsByYear[year].map(({ slug, title, date }, index) => (
-                <Link
-                  key={slug}
-                  href={`/blog/${slug}`}
-                  className="col-span-3 grid grid-cols-[100px_1fr_100px] gap-4 items-center"
-                >
-                  <div className="text-sm text-gray-500">
-                    {index === 0 && year}
-                  </div>
-                  <div className="text-gray-500">{title}</div>
-                  <div className="text-gray-500 text-sm">
-                    {dayjs(date).format("MM. DD.")}
-                  </div>
-                </Link>
-              ))}
-            </div>
-          );
-        })}
-      </div>
+      {years.map((year) => {
+        return (
+          <div
+            key={year}
+            className="grid grid-cols-[100px_1fr_100px] gap-4 border-t last:border-b border-gray-200 py-2"
+          >
+            {postsByYear[year].map(({ slug, title, date }, index) => (
+              <Link
+                key={slug}
+                href={`/blog/${slug}`}
+                className="col-span-3 grid grid-cols-[100px_1fr_100px] gap-4 items-center group"
+              >
+                <div className="text-sm text-gray-500 group-hover:text-gray-900 dark:group-hover:text-white transition-colors">
+                  {index === 0 && year}
+                </div>
+                <div className="text-gray-500 group-hover:text-gray-900 dark:group-hover:text-white transition-colors">
+                  {title}
+                </div>
+                <div className="text-gray-500 text-sm group-hover:text-gray-900 dark:group-hover:text-white transition-colors">
+                  {dayjs(date).format("MM. DD.")}
+                </div>
+              </Link>
+            ))}
+          </div>
+        );
+      })}
     </Container>
   );
 }
