90eb886de73d8a69426bac446fa3461acd4d09c9
Geuni <dlrmsgnl620@naver.com>
2025-02-01 16:05:19 +0900

    feat: main grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,21 +13,25 @@ export default async function Home() {
         return (
           <div
             key={year}
-            className="grid grid-cols-[100px_1fr_100px] gap-4 border-t last:border-b border-gray-200 py-2"
+            className="posts-group grid gap-4 border-t last:border-b border-gray-200 py-2"
           >
             {postsByYear[year].map(({ slug, title, date }, index) => (
               <Link
                 key={slug}
                 href={`/blog/${slug}`}
-                className="col-span-3 grid grid-cols-[100px_1fr_100px] gap-4 items-center group"
+                className="grid grid-cols-[100px_1fr_100px] gap-4 items-center"
               >
-                <div className="text-sm text-gray-500 group-hover:text-gray-900 dark:group-hover:text-white transition-colors">
+                <div
+                  className={`text-sm transition-colors ${
+                    index === 0 ? "year-label text-gray-500" : ""
+                  }`}
+                >
                   {index === 0 && year}
                 </div>
-                <div className="text-gray-500 group-hover:text-gray-900 dark:group-hover:text-white transition-colors">
+                <div className="text-gray-500 transition-colors hover:text-gray-900 dark:hover:text-white">
                   {title}
                 </div>
-                <div className="text-gray-500 text-sm group-hover:text-gray-900 dark:group-hover:text-white transition-colors">
+                <div className="text-gray-500 text-sm transition-colors hover:text-gray-900 dark:hover:text-white">
                   {dayjs(date).format("MM. DD.")}
                 </div>
               </Link>
