ebcf04db2fd4b46ddaac98c78a897956f2037978
Geuni <dlrmsgnl620@naver.com>
2025-03-15 22:38:31 +0900

    docs: flex:1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,9 @@ import tailwindcssAnimate from "tailwindcss-animate";
 export default {
   darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/content/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
