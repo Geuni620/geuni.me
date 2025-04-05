c1ef7e4aa6d6388f5f4289415e5f6c36a4a4ffd4
Geuni <dlrmsgnl620@naver.com>
2025-04-05 22:57:23 +0900

    fix: change children to file props

--- src/components/sandpack.tsx
+++ src/components/sandpack.tsx
@@ -1,43 +1,11 @@
-import React, { useMemo } from "react";
 import { githubLight } from "@codesandbox/sandpack-themes";
-import { Sandpack } from "@codesandbox/sandpack-react";
+import { Sandpack, type SandpackFiles } from "@codesandbox/sandpack-react";
 
 interface SandpackWrapperProps {
-  children: string;
+  files: SandpackFiles;
 }
 
-export const SandpackWrapper: React.FC<SandpackWrapperProps> = ({
-  children,
-}) => {
-  const files = useMemo(() => {
-    return children
-      .split("---")
-      .map((file) => file.trim())
-      .filter(Boolean)
-      .reduce(
-        (acc: Record<string, { code: string }>, file) => {
-          const lines = file.split("\n");
-          if (lines.length === 0) {
-            return acc;
-          }
-
-          // 첫 줄에서 파일명을 추출 (주석 표시 "//" 제거)
-          const fileName = lines[0].replace(/^\/\//, "").trim();
-          const code = lines.slice(1).join("\n").trim();
-
-          if (!fileName) {
-            console.warn(
-              "파일 이름이 누락되었습니다. 첫 줄에 '//파일명' 형식으로 작성해주세요."
-            );
-            return acc;
-          }
-
-          return { ...acc, [fileName]: { code } };
-        },
-        {} as Record<string, { code: string }>
-      );
-  }, [children]);
-
+export const SandpackWrapper: React.FC<SandpackWrapperProps> = ({ files }) => {
   return (
     <div className="relative lg:-mx-20 xl:-mx-24">
       <Sandpack
