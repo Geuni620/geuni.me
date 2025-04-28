34361ec1fdccb36982dd8b4c48bb2c83b47e0f09
Geuni <dlrmsgnl620@naver.com>
2025-04-29 00:54:43 +0900

    refc: change useEffect to callback ref at hander link

--- src/components/header-link-wrapper.tsx
+++ src/components/header-link-wrapper.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useEffect, useRef } from "react";
 
 export const HeaderLinkWrapper = ({
   children,
@@ -9,34 +8,27 @@ export const HeaderLinkWrapper = ({
   children: React.ReactNode;
 }) => {
   const router = useRouter();
-  const contentRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
+  const callbackRef = (node: HTMLDivElement) => {
     const handleClick = (event: MouseEvent) => {
       const target = event.target as HTMLElement;
 
-      // 클릭된 요소가 H1-H6이고 id 속성이 있으며 contentRef 내부에 있는지 확인
       if (
         target.tagName.match(/^H[1-6]$/) &&
         target.id &&
-        contentRef.current?.contains(target)
+        node.contains(target)
       ) {
         const hash = `#${target.id}`;
         router.push(hash);
       }
     };
 
-    const container = contentRef.current;
-    if (container) {
-      container.addEventListener("click", handleClick);
-    }
+    node.addEventListener("click", handleClick);
 
     return () => {
-      if (container) {
-        container.removeEventListener("click", handleClick);
-      }
+      node.removeEventListener("click", handleClick);
     };
-  }, [router]);
+  };
 
-  return <div ref={contentRef}>{children}</div>;
+  return <div ref={callbackRef}>{children}</div>;
 };
