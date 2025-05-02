cd24b118b7a0bf415d4b80a19aae7971404d5be8
Geuni <dlrmsgnl620@naver.com>
2025-05-02 15:04:27 +0900

    docs: 리액트 19에서 추가된 기능, ref clean up function

--- src/content/2025/05/01/file.ts
+++ src/content/2025/05/01/file.ts
@@ -1,67 +1,219 @@
 export const REACT_TEMPLATE_VITE_18 = {
   files: {
     "/index.html": {
-      code: `<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>React 18 + Vite</title>
-    </head>
-    <body>
-      <div id="root"></div>
-      <script type="module" src="/src/main.jsx"></script>
-    </body>
-  </html>`,
+      code: `<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vite + React + TS</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="/src/main.tsx"></script>
+  </body>
+</html>`,
     },
-    "/src/App.jsx": {
-      code: `export default function App() {
-    return <h1>Hello world (React 18)</h1>;
-  }`,
+    "/App.tsx": {
+      code: `import { useState } from "react";
+
+function App() {
+  const [showChild, setShowChild] = useState(true);
+
+  const toggleChild = () => {
+    setShowChild((prev) => !prev);
+  };
+
+  return (
+    <div>
+      <button onClick={toggleChild}>Toggle Child</button>
+      {showChild && <ChildComponent />}
+    </div>
+  );
+}
+
+const ChildComponent = () => {
+  const callbackRef = (node: HTMLDivElement) => {
+    if (node !== null) {
+      console.log("element is mounted");
+    } else {
+      console.log("element is null");
+    }
+
+    // 주석을 해제하면 Warning이 발생한다.
+    // return () => {
+    //   console.log("unmounted");
+    // };
+  };
+
+  return <div ref={callbackRef}>Child</div>;
+};
+
+export default App;
+`,
     },
-    "/src/main.jsx": {
-      code: `import React from "react";
-  import ReactDOM from "react-dom/client";
-  import App from "./App";
-  
-  ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );`,
-    },
-    "/vite.config.js": {
-      code: `import { defineConfig } from "vite";
-  import react from "@vitejs/plugin-react";
-  
-  export default defineConfig({
-    plugins: [react()],
-  });`,
+    "/main.tsx": {
+      code: `import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+`,
     },
     "/package.json": {
-      code: JSON.stringify(
-        {
-          name: "vite-react-18",
-          version: "1.0.0",
-          scripts: {
-            dev: "vite",
-            build: "vite build",
-            preview: "vite preview",
-          },
-          dependencies: {
-            react: "18.2.0",
-            "react-dom": "18.2.0",
-          },
-          devDependencies: {
-            vite: "^5.0.0",
-            "@vitejs/plugin-react": "^4.0.0",
-          },
-        },
-        null,
-        2
-      ),
+      code: `{
+  "name": "react-18",
+  "private": true,
+  "version": "0.0.0",
+  "type": "module",
+  "scripts": {
+    "dev": "vite",
+    "build": "tsc -b && vite build",
+    "lint": "eslint .",
+    "preview": "vite preview"
+  },
+  "dependencies": {
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0"
+  },
+  "devDependencies": {
+    "@types/react": "^18.3.20",
+    "@types/react-dom": "^18.3.7",
+    "@vitejs/plugin-react": "^4.3.4",
+    "eslint": "^8.57.1",
+    "eslint-plugin-react-hooks": "^4.6.2",
+    "eslint-plugin-react-refresh": "^0.4.19",
+    "globals": "^16.0.0",
+    "typescript": "~5.7.2",
+    "typescript-eslint": "^7.18.0",
+    "vite": "^6.3.1"
+  }
+}`,
+    },
+    "/vite.config.ts": {
+      code: `import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+`,
     },
   },
-  main: "/src/main.jsx",
-  environment: "vite",
+};
+
+export const REACT_TEMPLATE_VITE_19 = {
+  files: {
+    "/index.html": {
+      code: `<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vite + React + TS</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="/src/main.tsx"></script>
+  </body>
+</html>`,
+    },
+    "/App.tsx": {
+      code: `import { useState } from "react";
+
+function App() {
+  const [showChild, setShowChild] = useState(true);
+
+  const toggleChild = () => {
+    setShowChild((prev) => !prev);
+  };
+
+  return (
+    <div>
+      <button onClick={toggleChild}>Toggle Child</button>
+      {showChild && <ChildComponent />}
+    </div>
+  );
+}
+
+const ChildComponent = () => {
+  const callbackRef = (node: HTMLDivElement) => {
+    if (node !== null) {
+      console.log("element is mounted");
+    } else {
+      console.log("element is null");
+    }
+
+    return () => {
+      console.log("unmounted");
+    };
+  };
+
+  return <div ref={callbackRef}>Child</div>;
+};
+
+export default App;
+`,
+    },
+    "/main.tsx": {
+      code: `import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+`,
+    },
+    "/package.json": {
+      code: `{
+  "name": "react-19",
+  "private": true,
+  "version": "0.0.0",
+  "type": "module",
+  "scripts": {
+    "dev": "vite",
+    "build": "tsc -b && vite build",
+    "lint": "eslint .",
+    "preview": "vite preview"
+  },
+  "dependencies": {
+    "react": "^19.0.0",
+    "react-dom": "^19.0.0"
+  },
+  "devDependencies": {
+    "@eslint/js": "^9.22.0",
+    "@types/react": "^19.0.10",
+    "@types/react-dom": "^19.0.4",
+    "@vitejs/plugin-react": "^4.3.4",
+    "eslint": "^9.22.0",
+    "eslint-plugin-react-hooks": "^5.2.0", 
+    "eslint-plugin-react-refresh": "^0.4.19",
+    "globals": "^16.0.0",
+    "typescript": "~5.7.2",
+    "typescript-eslint": "^8.26.1",
+    "vite": "^6.3.1"
+  }
+}`,
+    },
+    "/vite.config.ts": {
+      code: `import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+`,
+    },
+  },
 };
